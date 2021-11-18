9e6ae82f3a80839c2cacaeb4e32da0f26d4377d8
Talo <talo90015@gmail.com>
2021-11-18 20:23:01 +0800

    Button、Toast、AlertDialog implement

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
 
@@ -14,6 +17,22 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnBMI.setOnClickListener {
+            bmi()
+        }
+    }
+    fun bmi(){
+        val weight = binding.editWeight.text.toString().toFloat()
+        val height = binding.editHeight.text.toString().toFloat()
+        val bmi = weight / (height * height)
+        Log.d("result", bmi.toString())
+
+        AlertDialog.Builder(this)
+            .setMessage(bmi.toString())
+            .setTitle("Reuslt")
+            .setPositiveButton("OK", null)
+            .show()
     }
 
 }
