9b9d64d28a009bb5b5637872817559349f1fa537
Talo <talo90015@gmail.com>
2021-11-22 22:01:41 +0800

    Activity Life cycle

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MainActivity ", "onCreate")
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -27,6 +28,36 @@ class MainActivity : AppCompatActivity() {
         binding.btnHelp.setOnClickListener {
             bmiHelpAlertDialog()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity ", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity ", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity ", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity ", "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MainActivity ", "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity ", "onDestroy")
     }
 
     fun bmi() {
